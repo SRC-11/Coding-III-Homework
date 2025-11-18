9a695eafcd645624cad0715dd66f802e94a98260
SRC-11 <sophia.cattelan@cuw.edu>
2025-11-17 21:45:33 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,108 +1,27 @@
-package main;
-
-import java.util.Random;
+package bibleCount;
 
 public class Main {
+
 	
 	public static void main(String[] args) {
-		int start = 0;
-		int end = 1000;
-		int[] arr = generate_random_unique(start, end);
-		int[] intervals = {1000, 500, 200, 1};
-		 insertionSort (arr);
-		    System.out.println("End of insertion sort.");
-	   ShellSort(arr, intervals);
-	   System.out.println("First shell sort complete.");
-	 ShellSort2(arr);
-	 System.out.println("Second shell sort complete");
+		int tru = 13;
+		int years = 0;
+		int exponent = 0;
+		long pop = 7699999987L;
+		findNumberofDisciples(pop);
+		while (pop > 0)
+		{
+			years = years + 3;
+			pop = pop - (tru * (2^exponent));
+			exponent = exponent + 1;
+		}
+		System.out.println("Answer 1) It will take" + " " + years + " " + "years.");
+	}
 
+
+public static void findNumberofDisciples(long pop) {
+	long answer = Math.round(Math.pow(pop, 1.0/16.0));
+	System.out.println("Answer 2) You would need to train" + " " + answer + " " + "disciples at a time.");
 }
-	
-	public static int[] generate_random_unique(int start, int end)
-	{
-		
-		int[] arr = new int[10000];
-		Random r = new Random();
-		for (int l = 0; l < 10000; l++)
-		{
-			arr[l] = r.nextInt(start, end);
-			for (int j = 0; j < 10000; j++)
-			{
-				if (arr[j] == arr[l] && l != j )
-				{
-					arr[l] = r.nextInt(start, end);
-					break;
-				}
-			}
-		} 
-	
-		
-		   return arr;	
-		
-	}
-	public static int[] insertionSort(int[] arr)
-	{
-		int[] recreate = new int [10000];
-		for (int v = 0; v < 10000; v++)
-		{
-			recreate[v] = arr[v];
-		}
-	    int c = 0;
-	    int d = 0;
-	    int temp3 = 0;
-	    long start3 = System.currentTimeMillis();
-	    for (c = 1; c < 10000; c++) {
-	    	d = c;
-	    	while (d > 0 && recreate[d] < recreate[d -1]) {
-	    		temp3 = recreate[d];
-	    		recreate[d] = recreate[d - 1];
-	    		recreate[d - 1] = temp3;
-	    		--d;
-	    	}
-	    }
-	    long finish3 = System.currentTimeMillis();
-	    long timeElapsed3 = finish3 - start3;
-	    System.out.println("Sorting an array of size 10000 took insertion sort" + " " + timeElapsed3 + " " + "ms to complete");
-		return arr;
-	}
-	public static int[] ShellSort(int[] arr, int[] intervals)
-	{
-		int k = 0;
-		int temp = 0;
-	  long start2 = System.currentTimeMillis();
-		for (int i = 0; i < 4; i++)
-		{
-			for (int start = 0; start < (intervals[i]); start++)
-			{
-			for (int r = start + intervals[i]; r < 10000; r = r + (intervals[i]))
-			{
-				k = r;
-				while(((k - intervals[i]) >= start) && arr[k] < arr[k - (intervals[i])])
-				{
-					temp = arr[k];
-			        arr[k] = arr[k - intervals[i]];
-					arr[k - (intervals[i])] = temp;
-					k -= intervals[i];
-				}
-			}
-			}
-		}
-		long finish2 = System.currentTimeMillis();
-	    long timeElapsed2 = finish2 - start2;
-	    System.out.println("Sorting an array of size 10000 took the shell sort" + " " + timeElapsed2 + " " + "ms to complete");
-		return arr;
-	}
-	public static int[] ShellSort2 (int[] arr)
-	{
-		int intervals[] = new int[14];
-		for (int i = 0; i < 14; i++)
-		{
-			int decide = 13;
-			intervals[i] = 2^decide;
-			decide = decide - 1;
-		}
-		ShellSort(arr, intervals);
-		return arr;
-		
-	}
 }
+
